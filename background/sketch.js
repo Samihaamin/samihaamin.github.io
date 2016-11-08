8a77a25176473e300f4a451e6c71d6275933242e
Samiha Amin <samiha.amin@rutgers.edu>
2016-11-08 13:21:20 -0500

    Background Redone

--- background/sketch.js
+++ background/sketch.js
@@ -1,4 +1,4 @@
-function setup() {
+/*function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
@@ -11,13 +11,40 @@ function draw() {
   }
   //ellipse(mouseX, mouseY, 400, 400);
   if (mouseY > windowWidth/3){
-  	ellipse(mouseX, mouseY+100, 300, 300);
+  	ellipse(mouseX, mouseY+100, 500, 500);
   }
 
   if (mouseX > windowWidth/2){
-  ellipse(mouseX-60, mouseY, 300, 300); }
+  ellipse(mouseX-60, mouseY, 500, 500); }
   else if (mouseX < windowWidth/2){
-  	  ellipse(mouseX+60, mouseY, 300, 300);
+  	  ellipse(mouseX+60, mouseY, 500, 500);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}  */
+
+var totalPts = 200;
+var steps = totalPts + 1;
+  
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  stroke(255);
+  frameRate(1.5);
+} 
+
+function draw() {
+  background(255);
+  var rand = 0;
+  for  (var i = 1; i < steps; i++) {
+    fill('#FFE4E1');
+    ellipse( ((width/steps) * i), ((height/2) + random(-rand, rand)), 10,10 );
+    fill(255);
+    ellipse( ((width/steps) * i), ((height/2) + random(-rand, rand)), 6,16 );
+    fill(240);
+    ellipse( ((width/steps) * i), ((height/2) + random(-rand, rand)), 6,6 );
+    rand += random(-65, 65);
   }
 }
 
