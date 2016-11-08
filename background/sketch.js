a2a516a714ad5f5e3043584aff078fbf8363432d
Samiha Amin <samiha.amin@rutgers.edu>
2016-11-07 23:49:05 -0500

    Background smoother

--- background/sketch.js
+++ background/sketch.js
@@ -9,7 +9,16 @@ function draw() {
   } else {
     fill('#FFE4E1');
   }
-  ellipse(mouseX, mouseY, 350, 350);
+  //ellipse(mouseX, mouseY, 400, 400);
+  if (mouseY > windowWidth/3){
+  	ellipse(mouseX, mouseY+100, 300, 300);
+  }
+
+  if (mouseX > windowWidth/2){
+  ellipse(mouseX-60, mouseY, 300, 300); }
+  else if (mouseX < windowWidth/2){
+  	  ellipse(mouseX+60, mouseY, 300, 300);
+  }
 }
 
 function windowResized() {
